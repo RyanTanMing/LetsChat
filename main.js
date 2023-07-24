125dbc11bef9cbbb1824d95fa159e20733c305c4
RyanTanMing <125744706+RyanTanMing@users.noreply.github.com>
2023-07-24 16:33:48 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCmV4HZ_0wELw6giOaldpwxgt66qnElcEw",
+    authDomain: "letschat-f2097.firebaseapp.com",
+    databaseURL: "https://letschat-f2097-default-rtdb.firebaseio.com",
+    projectId: "letschat-f2097",
+    storageBucket: "letschat-f2097.appspot.com",
+    messagingSenderId: "1024353378123",
+    appId: "1:1024353378123:web:f30d06d591139d35ec92ce"
+  };
+  
+  // Initialize Firebase
+   firebase.initializeApp(firebaseConfig);
 const signUpButton = document.getElementById('signUp');
 const signInButton = document.getElementById('signIn');
 const container = document.getElementById('container');
