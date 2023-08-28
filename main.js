67ae6d45c1a58c453763f99c887ca8649c358f7b
RyanTanMing <125744706+RyanTanMing@users.noreply.github.com>
2023-08-28 20:25:51 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -14,15 +14,11 @@ const signUpButton = document.getElementById('signUp');
 const signInButton = document.getElementById('signIn');
 const container = document.getElementById('container');
 
-signUpButton.addEventListener('click', () => {
-	container.classList.add("right-panel-active");
-});
 
-signInButton.addEventListener('click', () => {
-	container.classList.remove("right-panel-active");
-});
+
+
 function adduser() {
     user=document.getElementById("email").value 
     localStorage.setItem("user",user)
-    window.location="second.html"
+    window.location="kwitter_room.html"
 }
